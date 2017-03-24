e345bec7330de290bb491bf14c00ddcd25c87f61
L.J.Graham <lg1u16@cyan01.(none)>
2017-03-24 09:26:23 +0000

    moved run script into main folder to work with the functions script

--- two_step_binary.py
+++ two_step_binary.py
@@ -6,13 +6,16 @@ Created on Thu Mar 23 15:16:00 2017
 """
 import pandas as pd
 import os
-os.chdir('SCALEFORES/1_Simulated_Landscapes')
+#import sys
+#sys.path.append("/home/lg1u16/1_Simulated_Landscapes")
+#os.chdir('/home/lg1u16/1_Simulated_Landscapes')
 
 import nlmfunctions as nlm
 
 
 # gets the number of the job array - use this to select the line from the params file
-job_no = os.getenv('PBS_ARRAY_INDEX') - 1
+#job_no = os.getenv('PBS_ARRAY_INDEX') - 1
+job_no = 1
 
 params = pd.read_csv("two_step_binary/params.csv")
 
@@ -20,4 +23,8 @@ params = params.ix[job_no]
 
 out = nlm.two_step_binary(50, params['p'], params['h'], params['w1'], params['w2'], params['f1'], params['f2'])
 
+if not os.path.exists('two_step_binary/results'):
+	os.makedirs('two_step_binary/results')
+
 out.to_csv('two_step_binary/results/output'+str(job_no)+'.csv', index=False)
+
