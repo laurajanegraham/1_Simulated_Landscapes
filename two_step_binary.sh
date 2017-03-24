167ece28e80ffaf38e209a336e36c9b41e4103c4
laurajanegraham <laurajaneegraham@gmail.com>
2017-03-24 16:16:49 +0000

    updated shell file

--- two_step_binary.sh
+++ two_step_binary.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 #PBS -N tsb
-#PBS -l walltime=00:05:00
-#PBS -t 0-6561
+#PBS -l walltime=00:15:00
+#PBS -t 1-100
 
 cd $PBS_O_WORKDIR
 
 module load python
 module load numpy
 
-python two_step_binary.py $PBS_ARRAYID
+python two_step_binary.py
